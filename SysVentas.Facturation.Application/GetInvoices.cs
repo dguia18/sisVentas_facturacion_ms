e9dbd5ca7df63bc5d9cb14a62c6e6f60113c32a9
Duvan Guia <dguia18@unicesar.edu.co>
2022-05-24 18:19:31 -0500

    arreglando consulta

--- SysVentas.Facturation.Application/GetInvoices.cs
+++ SysVentas.Facturation.Application/GetInvoices.cs
@@ -13,23 +13,23 @@ public class GetInvoices : IRequestHandler<GetInvoices.Request, GetInvoices.Resp
     }
     public Task<Response> Handle(Request request, CancellationToken cancellationToken)
     {
-        return Task.FromResult(new Response(LoadInvoices().Select(MapMaster())));
+        return Task.FromResult(new Response(LoadInvoices().Select(MapMaster)));
     }
-    private IQueryable<InvoiceMaster> LoadInvoices()
+    private IEnumerable<InvoiceMaster> LoadInvoices()
     {
         return _unitOfWork.GenericRepository<InvoiceMaster>().FindBy(includeProperties: "Details");
     }
-    private static Expression<Func<InvoiceMaster, InvoiceMasterModelView>> MapMaster()
+    private static InvoiceMasterModelView MapMaster(InvoiceMaster t)
     {
-        return t => new InvoiceMasterModelView(t.ClientId, t.Date, t.StatusView, t.Total, t.Details
-            .Select(MapDetail()));
+        return new InvoiceMasterModelView(t.ClientId, t.Date, t.StatusView, t.Total, t.Details
+            .Select(MapDetail));
     }
-    private static Func<InvoiceDetail, InvoiceDetailModelView> MapDetail()
+    private static InvoiceDetailModelView MapDetail(InvoiceDetail d)
     {
-        return d => new InvoiceDetailModelView(d.ProductId, d.Quantity, d.Price, d.Total);
+        return new InvoiceDetailModelView(d.ProductId, d.Quantity, d.Price, d.Total);
     }
-    public record Request() : IRequest<Response>;
-    public record Response(IEnumerable<InvoiceMasterModelView> invoices);
+    public record Request : IRequest<Response>;
+    public record Response(IEnumerable<InvoiceMasterModelView> Invoices);
     public record InvoiceMasterModelView(long ClientId, DateTime Date, string Status, decimal Total, IEnumerable<InvoiceDetailModelView> Details);
     public record InvoiceDetailModelView(long ProductId, decimal Quantity, decimal Price, decimal Total);
 }
